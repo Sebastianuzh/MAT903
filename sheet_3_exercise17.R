30200c9ea369b28a37a5e825526ec5f659bbb02c
Sebastian Schwarzenbach <sebastian.schwarzenbach@uzh.ch>
2025-03-06 11:56:00 +0100

    fertig

--- sheet_3_exercise17.R
+++ sheet_3_exercise17.R
@@ -10,7 +10,7 @@ print(spring_weights)
 bin_width <- 1 
 origin <-5
 break_seq<- seq(origin, max(spring_weights) + bin_width, by = bin_width)
-
+dev.new()
 hist(spring_weights, breaks = breaks_seq, col = "lightblue", border = "black", main = "Histogram of Spring Weights bins 1", xlab = "Maximal Weight (tons)")
 
 #Histogram with bin width h = 0.5 and origin k = 5
@@ -18,6 +18,12 @@ hist(spring_weights, breaks = breaks_seq, col = "lightblue", border = "black", m
 bin_width_2 <- 0.5
 
 breaks_seq_2<- seq(origin, max(spring_weights) + bin_width_2, by = bin_width_2)
-
+dev.new()
 hist(spring_weights, breaks = breaks_seq_2, col = "lightblue", border = "black", main = "Histogram of Spring Weights bins 0.5", xlab = "Maximal Weight (tons)")
 
+
+# Calculate the 75th percentile (third quartile)
+q3 <- quantile(spring_weights, 0.75)
+
+# Print the result
+print(q3)
